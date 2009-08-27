19c7522638a103af3f89dd9abae132d5bf20faa8
trond <trond@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-27 13:31:20 +0000

    A major update of this script that calculates averages for all individuals and also plots standard deviation as grey shades

--- plotStation.py
+++ plotStation.py
@@ -39,7 +39,7 @@ sgrrel=cdf.variables["sgr_rel"][:,:,:,:]
 
 fig=plt.figure()
 ax = fig.add_subplot(3,1,1)
-colors=('r','g','b','c','y','m','k')
+colors=('r','g','b','c','y','m','k','r','g','b','c','y','m','k','r','g','b','c','y','m','k')
 
 """Calculate 24 hour running mean of specific growth rate"""
 Ker = np.ones((24,), dtype='float')
@@ -47,35 +47,44 @@ Ker[0], Ker[-1] = 0.5, 0.5
 Ker = Ker/(24.)
 depth24H=np.zeros(depth.shape,dtype=float64)
 
-Ncohorts=7#len(depth[:,0,0,0])-1
+Ncohorts=11#len(depth[:,0,0,0])-4
 Nindividuals=len(depth[0,:,0,0])
 print "Number of individuals %s and cohorts %s"%(Nindividuals, Ncohorts)
 for cohort in range(Ncohorts):
     index1=int(timeIndex[cohort,0,0,0])+1
     index2=int(timeIndex[cohort,0,1,0])-1
     
-    days=(index2-index1)/24 
+    days=int((index2-index1)/24)
+    print "Finding %s days period of growths for cohort %s"%(days,cohort)
     
-    meanSGR24H=np.zeros((14),dtype=float64)
-    meanIndSGR24H=np.zeros((Nindividuals,14),dtype=float64)
+    meanSGR24H=np.zeros((days),dtype=float64)
+    meanIndSGR24H=np.zeros((Nindividuals,days),dtype=float64)
     
-    stdSGR24H=np.zeros((14),dtype=float64)
+    stdSGR24H=np.zeros((days),dtype=float64)
      
-    time24H=np.zeros((14),dtype=float64)
+    time24H=np.zeros((days),dtype=float64)
     
     for i in range(days):
         k1=index1+i*24
         k2=index1+(i+1)*24
-        
-        meanSGR24H[i]=sum(sgr[cohort,:,k1:k2,0])/Nindividuals
-        meanIndSGR24H[:,i]=sum(std(sgr[cohort,:,k1:k2,0]))
-        
-        stdSGR24H[i]=std(meanIndSGR24H)
-        time24H[i]=time[k1]
        
+        meanSGR24H[i]=sum(sgr[cohort,:,k1:k2,0])/Nindividuals
+     
+        j=0
+        
+        for j in range(Nindividuals):
+            meanIndSGR24H[j,i]=sum(sgr[cohort,j,k1:k2,0])
+        
+        stdSGR24H[i]=std(meanIndSGR24H[:,i])
+        time24H[i]=time[k1]
+    
+    stdMinus = meanSGR24H - stdSGR24H
+    stdPlus  = meanSGR24H + stdSGR24H
+    
  #   sgr24H[cohort,ind,index1:index2,0]= np.convolve(sgr[cohort,ind,index1:index2,0], Ker, mode='same')
-    ax.plot(time24H,meanSGR24H,colors[cohort],linewidth = 2)
-  
+    ax.plot(time24H,meanSGR24H,colors[cohort],linewidth = 3)
+    ax.fill_between(time24H, stdMinus, stdPlus, facecolor='black',alpha=0.5)
+
     #ax.plot(time[index1:index2],sgr[cohort,ind,index1:index2,0],colors[cohort],linewidth = 2)
     
 ax.set_title('Specific growth rate')
@@ -83,22 +92,31 @@ ax.set_title('Specific growth rate')
 
 ax = fig.add_subplot(3,1,2)
 for cohort in range(Ncohorts):
-    for ind in range(len(depth[0,:,0,0])):
-        index1=int(timeIndex[cohort,0,0,0])+1
-        index2=int(timeIndex[cohort,0,1,0])-1
-        ax.plot(time[index1:index2],wgt[cohort,ind,index1:index2,0],colors[cohort],linewidth = 2)
+
+     index1=int(timeIndex[cohort,0,0,0])+1
+     index2=int(timeIndex[cohort,0,1,0])-1
+     meanWGT = mean(wgt[cohort,:,index1:index2,0],0)
+     
+     ax.plot(time[index1:index2],meanWGT,colors[cohort],linewidth = 2)
+     stdMinus=mean(wgt[cohort,:,index1:index2,0],0) - std(wgt[cohort,:,index1:index2,0],0)
+     stdPlus=mean(wgt[cohort,:,index1:index2,0],0) + std(wgt[cohort,:,index1:index2,0],0)
+     ax.fill_between(time[index1:index2], stdMinus, stdPlus, facecolor='black',alpha=0.5)
+     
 ax.set_title('Weight')
 
 
 ax = fig.add_subplot(3,1,3)
 for cohort in range(Ncohorts):#len(depth[:,0,0,0])-1)
-    for ind in range(len(depth[0,:,0,0])):
         
-        index1=int(timeIndex[cohort,0,0,0])+1
-        index2=int(timeIndex[cohort,0,1,0])-1
-        depth24H[cohort,ind,index1:index2,0]= np.convolve(depth[cohort,ind,index1:index2,0], Ker, mode='same')
-        
-        ax.plot(time[index1:index2],-depth24H[cohort,ind,index1:index2,0],colors[cohort],linewidth = 2)
+    index1=int(timeIndex[cohort,0,0,0])+1
+    index2=int(timeIndex[cohort,0,1,0])-1
+    meanDEPTH = mean(-depth[cohort,:,index1:index2,0],0)
+ 
+    ax.plot(time[index1:index2],meanDEPTH,colors[cohort],linewidth = 2)
+    stdMinus=mean(-depth[cohort,:,index1:index2,0],0) - std(-depth[cohort,:,index1:index2,0],0)
+    stdPlus=mean(-depth[cohort,:,index1:index2,0],0) + std(-depth[cohort,:,index1:index2,0],0)
+    ax.fill_between(time[index1:index2], stdMinus, stdPlus, facecolor='black',alpha=0.5)
+    
 ax.set_title('Weight')
 
 
