d0b686cd2ad653eb77ca292d598342fc77246144
trond <trond@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-11-30 11:41:06 +0000

    New class to speed  up code using cache and memoize

---
+++ memoize.py
@@ -0,0 +1,13 @@
+# memoize.py
+
+def memoize(function):
+    cache = {}
+    def decorated_function(*args):
+        
+        if args in cache:
+            return cache[args]
+        else:
+            val = function(*args)
+            cache[args] = val
+            return val
+    return decorated_function
